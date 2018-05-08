5abd71623a497ade1a79305109ca3df83a9d9393
whwhdtk5689 <whwhdgus5689@naver.com>
2018-05-08 09:22:54 +0900

    dd

--- Together/src/main/java/yjp/wdb/together/HomeController.java
+++ Together/src/main/java/yjp/wdb/together/HomeController.java
@@ -106,7 +106,7 @@ public class HomeController {
 		if (l.getStatus() == 1) {
 			System.out.println("dd");
 			session.setAttribute("login", login);
-			return "main";
+			return "redirect:main";
 		} else {
 			System.out.println("nn");
 			return "redirect:login";
